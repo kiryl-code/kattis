30c68fe73e7ce6f716d0070fdd9068857f84998e
Kiryl Yasny <kiya2400@student.miun.se>
2026-06-15 15:27:27 +0200

    Add solution

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,25 @@
+import java.util.Scanner;
+
 public final class Main {
     private Main() { throw new IllegalStateException("Utility class"); }
 
-    static void main(String[] args) {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String firstCode = scanner.nextLine();
+        String secondCode = scanner.nextLine();
 
+        // in a real world solution should check if input is of type int
+
+        if (firstCode.equalsIgnoreCase(secondCode)) { System.out.println(1); return; }
+
+        int maxCombos = 16;
+        for (int i = 0; i < 4; i ++) {
+            char firstChar = firstCode.charAt(i);
+            char secondChar = secondCode.charAt(i);
+            if (firstChar == secondChar) {
+                maxCombos = maxCombos / 2;
+            }
+        }
+        System.out.println(maxCombos);
     }
 }
