9a33cb5fd0d18880268eda169c0bcf1c6eca4985
Kiryl Yasny <kiya2400@student.miun.se>
2026-06-22 14:12:38 +0200

    Add solution

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,24 @@
+import java.util.Scanner;
+
 public final class Main {
     private Main() { throw new IllegalStateException("Utility class"); }
 
     public static void main(String[] args) {
-
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.nextLine().trim();
+        int sum = 0;
+        boolean shouldContinue = true;
+        while (shouldContinue) {
+            for (char c : input.toCharArray()) {
+                sum += Character.getNumericValue(c);
+            }
+            if (sum < 10) {
+                shouldContinue = false;
+            } else {
+                input = String.valueOf(sum);
+                sum = 0;
+            }
+        }
+        System.out.println(sum);
     }
 }
