0c78a695db599de7677b3cf8703064febc5dc8c1
Kiryl Yasny <kiya2400@student.miun.se>
2026-06-22 16:02:54 +0200

    Add solution

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,18 @@
+import java.util.Scanner;
+
 public final class Main {
     private Main() { throw new IllegalStateException("Utility class"); }
 
     public static void main(String[] args) {
-
+        Scanner scanner = new Scanner(System.in);
+        String machineCode = scanner.nextLine().trim();
+        int additionalNOPs = 0;
+        for (int i = 0; i < machineCode.length(); i++) {
+            char c = machineCode.charAt(i);
+            if (Character.isUpperCase(c) && (i + additionalNOPs) % 4 != 0) {
+                    additionalNOPs += 4 - (i + additionalNOPs) % 4;
+            }
+        }
+        System.out.println(additionalNOPs);
     }
 }
