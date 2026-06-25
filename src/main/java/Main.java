041459ae1a0267f0d0301f9879eed251e84851f6
Kiryl Yasny <kiya2400@student.miun.se>
2026-06-25 13:50:55 +0200

    Add solution

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,20 @@
+import java.util.Scanner;
+
 public final class Main {
+    private static final String vowels = "aeiou";
     private Main() { throw new IllegalStateException("Utility class"); }
 
     public static void main(String[] args) {
-
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.nextLine().trim();
+        int splitPos = input.length() - 1;
+        for (int i = input.length() - 1; i >= 0; i--) {
+            if (vowels.contains(input.substring(i, i + 1).toLowerCase())) {
+               splitPos = i + 1;
+               break;
+            }
+        }
+        String inputSub = input.substring(0, splitPos);
+        System.out.println(inputSub + "ntry");
     }
 }
